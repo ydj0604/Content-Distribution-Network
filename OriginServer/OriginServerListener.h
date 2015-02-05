29ef3022f9d31731701e4a212bac4ceec26448e9
Dong Jin Yu <yudj0604@gmail.com>
2015-02-04 22:25:26 -0800

    Origin Server Impl Pseudo-code

---
+++ OriginServer/OriginServerListener.h
@@ -0,0 +1,11 @@
+#ifndef ORIGIN_SERVER_LISTENER_H
+#define ORIGIN_SERVER_LISTENER_H
+
+class OriginServerListener {
+public:
+
+private:
+	OriginServer* m_origin;
+};
+
+#endif
---
+++ OriginServer/OriginServerSender.h
@@ -0,0 +1,15 @@
+#ifndef ORIGIN_SERVER_SENDER_H
+#define ORIGIN_SERVER_SENDER_H
+
+#include "OriginServer.h"
+
+using namespace std;
+
+class OriginServerSender {
+public:
+
+private:
+	OriginServer* m_origin;
+};
+
+#endif
