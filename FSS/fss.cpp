68bda16330165101bcbb9952e7094a5cd52945b0
Seena Burns <hello@seenaburns.com>
2015-02-15 17:00:26 -0800

    Add outline for FSS

---
+++ FSS/fss.cpp
@@ -0,0 +1,16 @@
+#include "fss.h"
+
+FSS::FSS() {
+}
+
+FSS::~FSS() {
+}
+
+void FSS::listen() {
+  while (1) {
+    // listen for http
+    // either
+    // save file
+    // or respond with file contents
+  }
+}
---
+++ FSS/fss.h
@@ -0,0 +1,10 @@
+#ifndef CDN_FSS_H
+#define CDN_FSS_H
+
+class FSS {
+public:
+  FSS();
+  ~FSS();
+  void listen();
+};
+#endif
