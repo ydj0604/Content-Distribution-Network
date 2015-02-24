065e4a1f44a4e86066e3eb50695b29c61aac4ba0
Seena Burns <hello@seenaburns.com>
2015-02-23 19:42:20 -0800

    FSS responds to get request with 404 or 200 and file contents

--- FSS/fss.cpp
+++ FSS/fss.cpp
@@ -35,12 +35,42 @@ void FSS::listen() {
 void FSS::handle_get(http_request message) {
   /*
    * Envoked when cdn would like to recieve a file
+   *
+   * Responds with either
+   *  200 OK, and file contents
+   *  404 Not found, if requested file does not exist
    */
   string fileName = message.relative_uri().to_string();
+  string filePath = FSS_DIR + fileName;
+
+  bool fileExists = has_file(filePath);
 
   // Diagnostics
+  cout << endl << "---------------"<< endl;
+  cout << filePath << endl;
   cout << message.to_string() << endl;
-  cout << fileName << endl;
+  cout << "File exists: " << fileExists << endl;
 
-  message.reply(status_codes::OK, "test");
+  if (fileExists)
+    message.reply(status_codes::OK, get_file_contents(filePath));
+  else
+    message.reply(status_codes::NotFound, "");
+}
+
+bool FSS::has_file(string filePath) {
+  ifstream file(filePath.c_str());
+  if (!file)
+    return false;
+  else
+    return true;
+}
+
+string FSS::get_file_contents(string filePath) {
+  // Return contents of file
+  // Assumes check for file existing already handled
+  ifstream f(filePath);
+  stringstream buf;
+  buf << f.rdbuf();
+
+  return buf.str();
 }
--- FSS/fss.h
+++ FSS/fss.h
@@ -3,12 +3,14 @@
 
 #include "cpprest/http_listener.h"
 
+using namespace std;
 using namespace web;
 using namespace utility;
 using namespace http;
 using namespace web::http::experimental::listener;
 
 #define FSS_ADDR "http://localhost:5000/"
+#define FSS_DIR "../../testdir"
 
 class FSS {
 public:
@@ -16,9 +18,14 @@ public:
   ~FSS();
   void listen();
 private:
+  // Request function
   void handle_get(http_request message);
   void handle_put(http_request message);
   http_listener get_listener;
   http_listener put_listener;
+
+  // Other utility
+  bool has_file(string filePath);
+  string get_file_contents(string filePath);
 };
 #endif
