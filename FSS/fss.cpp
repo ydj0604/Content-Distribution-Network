eadd8ce1bfb26e1328a56e3ea551e560ec5ffab9
Seena Burns <hello@seenaburns.com>
2015-02-23 19:00:47 -0800

    Add basic get handling for FSS, listens with cpprest and outputs to console

--- FSS/fss.cpp
+++ FSS/fss.cpp
@@ -1,9 +1,24 @@
 #include "fss.h"
+#include "cpprest/http_listener.h"
+#include <iostream>
+
+using namespace std;
+using namespace web;
+using namespace utility;
+using namespace http;
+using namespace json;
+using namespace web::http::experimental::listener;
 
 FSS::FSS() {
+  string addr = "http://localhost:5000/";
+  get_listener = http_listener(addr);
+  get_listener.support(methods::GET, std::bind(&FSS::handle_get, this, std::placeholders::_1));
+  cout << "listening at address " << addr << endl;
+  get_listener.open().wait();
 }
 
 FSS::~FSS() {
+  get_listener.close();
 }
 
 void FSS::listen() {
@@ -13,4 +28,15 @@ void FSS::listen() {
     // save file
     // or respond with file contents
   }
+}
+
+void FSS::handle_get(http_request message) {
+  /*
+   * Envoked when cdn would like to recieve a file
+   */
+  string fileName = message.relative_uri().to_string();
+
+  // Diagnostics
+  cout << message.to_string() << endl;
+  cout << fileName << endl;
 }
--- FSS/fss.h
+++ FSS/fss.h
@@ -1,10 +1,22 @@
 #ifndef CDN_FSS_H
 #define CDN_FSS_H
 
+#include "cpprest/http_listener.h"
+
+using namespace web;
+using namespace utility;
+using namespace http;
+using namespace web::http::experimental::listener;
+
 class FSS {
 public:
   FSS();
   ~FSS();
   void listen();
+private:
+  void handle_get(http_request message);
+  void handle_put(http_request message);
+  http_listener get_listener;
+  http_listener put_listener;
 };
 #endif
