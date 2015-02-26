d4bef9915fecd90d77a801a70ef6bd02d863cd54
Steve Oh <steve.ohmn@gmail.com>
2015-02-26 02:12:58 -0800

    Complete code for ipToLatLng of getting geographic latitude/longitude

--- ipToLatLng/main.cpp
+++ ipToLatLng/main.cpp
@@ -1,14 +1,24 @@
+#include "cpprest/http_client.h"
 #include <iostream>
-#include "ipToLatLng.h"
-//#include "cpprest/http_listener.h"
 #include <string>
+#include "ipToLatLng.h"
 
 using namespace std;
+using namespace web;
+using namespace utility;
+using namespace http;
+using namespace json;
 
 int main() {
     ipToLatLng* ip_instance = new ipToLatLng();
 
     cout << ip_instance->getipaddr() << endl;
+
+    // using function IPJsonToLatLng(), obtain latitude, longitude from GET method
+    ip_instance->IPJsonToLatLng( ip_instance->getipaddr() );
+
+    cout << "Obtained latitude is: " << ip_instance->getlat() << endl;
+    cout << "Obtained longitude is: " << ip_instance->getlng() << endl;
 
     return 0;
 }
