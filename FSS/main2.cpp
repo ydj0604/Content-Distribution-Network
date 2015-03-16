f295a4443032599e7c0fb4f4d2b670494807d71e
Seena Burns <hello@seenaburns.com>
2015-03-15 22:45:43 -0700

    FSS reports what meta address is if provided as arg

--- FSS/main2.cpp
+++ FSS/main2.cpp
@@ -5,10 +5,15 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 	string metaAddr = "localhost:4000";
+
+  cout << "INITIALIZING FSS" << endl;
+
 	if(argc > 1) {
 		 string temp(argv[1]);
 		 metaAddr = temp;
 	}
+
+  cout << "meta ip: " << metaAddr << endl;
 	FSS* fss = new FSS(metaAddr);
 	fss->listen();
 	return 0;
