c8a09a5eb6ecff40a579b0636657c93ac10d8870
ydj0604 <ydj1989@gmail.com>
2015-03-16 02:01:24 -0700

    make location optional

--- CDN/main.cpp
+++ CDN/main.cpp
@@ -18,15 +18,17 @@ using namespace concurrency;
 
 int main(int argc, char *argv[]) {
 	string cdnIpAddr = "localhost:2000", metaIpAddr="localhost:4000", fssIpAddr="localhost:5000", loc="la";
-	if(argc > 4) {
+	if(argc > 3) {
 		string temp(argv[1]);
 		cdnIpAddr = temp;
 		string temp1(argv[2]);
 		metaIpAddr = temp1;
 		string temp2(argv[3]);
 		fssIpAddr = temp2;
+	}
+	if(argc > 4) {
 		string temp3(argv[4]);
-		loc = temp3;
+		loc = temp;
 	}
 
 	CDN_Node* cdn = new CDN_Node(cdnIpAddr, metaIpAddr, fssIpAddr, loc);
